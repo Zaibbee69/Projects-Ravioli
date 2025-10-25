15334f4cc01ea0273a5358164e446fc65104f6d4
Zaibbee69 <jahanzaib12569@gmail.com>
2025-10-25 18:13:12 +0500

    Made Movie Wind Submodule

--- InternIntelligence_MovieWebsite/movie_website/main_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, movie_details, search_results
-
-urlpatterns = [
-    path("", index, name="index"),
-    path("movie/<int:movie_id>/", movie_details, name="movie_details"),
-    path("search/", search_results, name="search_results"),
-]
