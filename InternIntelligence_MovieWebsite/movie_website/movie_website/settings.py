a43c2b3db7a53fa22416df1eb97eed8071c5d36b
Zaibbee69 <jahanzaib12569@gmail.com>
2025-10-25 18:03:08 +0500

    Prepared For Deployment

--- InternIntelligence_MovieWebsite/movie_website/movie_website/settings.py
+++ InternIntelligence_MovieWebsite/movie_website/movie_website/settings.py
@@ -33,9 +33,9 @@ TMDB_API_KEY = env("TMDB_API_KEY", default="")  # Default prevents errors if mis
 SECRET_KEY = 'django-insecure-zc&es2nx363r_3qy%*2wc#i5=gf@1_zik)fm@2+)ap2kbxxcvd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
